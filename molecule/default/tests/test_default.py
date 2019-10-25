c06440a7574aaa5f4b5a80ec26a52a143a09bf10
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2019-10-25 20:34:57 +0200

    Add some tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -7,9 +8,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_pref_file(host):
+    f = host.file('/etc/apt/preferences.d/docker-ce.pref')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+@pytest.mark.parametrize('pkg', [
+  'docker-ce',
+  'docker-ce-cli'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+
+def test_docker_service(host):
+    service = host.service('docker.service')
+
+    assert service.is_running
+    assert service.is_enabled
